ffa9923b86970468c679cc7a0836764956504c6c
Patrick Cash <PatrickCash@gmail.com>
2019-01-28 16:06:26 -0500

    added a placeholder viz and table

--- public-data-explorer/src/containers/UserData.js
+++ public-data-explorer/src/containers/UserData.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardHeader, CardBody, CardText, CardLink } from 'reactstrap';
+import { Card, CardHeader, CardBody, CardText, CardLink, Table } from 'reactstrap';
 import { connect } from 'react-redux';
 import isEmpty from 'lodash/isEmpty';
 
@@ -20,6 +20,37 @@ class UserData extends Component {
           "\"" + tag + "\" " 
         )}
         </CardText>
+        <img width="100%" src={require('../assets/placeholder_visualization.png')} alt="User data visualization"/>
+        <Table bordered striped>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Column A</th>
+              <th>Column B</th>
+              <th>Column C</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Row 1</th>
+              <td>A1</td>
+              <td>B1</td>
+              <td>C1</td>
+            </tr>
+            <tr>
+              <th scope="row">Row 2</th>
+                <td>A2</td>
+                <td>B2</td>
+                <td>C2</td>
+              </tr>
+            <tr>
+              <th scope="row">Row 3</th>
+              <td>A3</td>
+              <td>B3</td>
+              <td>C3</td>
+            </tr>
+          </tbody>
+        </Table>
         <CardLink href={dataEntry.download_url}>Download Data</CardLink>
         <hr/>
       </div>
